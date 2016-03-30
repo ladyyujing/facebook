eaa4189270322bb867496d09c00c4132a5b6853f
yujing <ladyyujing@163.com>
2016-03-30 21:12:41 +0800

    no message

--- facebook/AppDelegate.m
+++ facebook/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [[FBSDKApplicationDelegate sharedInstance] application:application
                              didFinishLaunchingWithOptions:launchOptions];
-   ////////////////////
+   
     return YES;
 }
 
